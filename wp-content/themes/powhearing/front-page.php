b75a2d08238cf42bd3754526afe577927729650b
zooropa <yosrismail@gmail.com>
2017-09-13 16:52:49 -0400

    about+footer

---
+++ wp-content/themes/powhearing/front-page.php
@@ -0,0 +1,29 @@
+<?php
+
+echo"front-page";
+
+
+
+
+?>
+
+<?php
+
+if(get_field('about_heading'))
+{
+	echo '<h4>' . get_field('about_heading') . '</h4>';
+}
+
+$about_pic = get_field('about_pic');
+
+$image = wp_get_attachment_image_src(get_field('image_test'), 'full'); ?>
+<img src="<?php echo $about_pic; ?>" alt="<?php echo get_the_title(get_field('image_test')) ?>" />
+
+<?php
+
+if(get_field('about_text'))
+{
+	echo '<p>' . get_field('about_text') . '</p>';
+}
+
+?>
