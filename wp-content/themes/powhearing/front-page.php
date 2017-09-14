0a847581688c2a7a920385e6b20e839c5479c2b3
shovalsolo <shovalso@gmail.com>
2017-09-14 15:00:28 -0400

    adding pages and components

--- wp-content/themes/powhearing/front-page.php
+++ wp-content/themes/powhearing/front-page.php
@@ -3,9 +3,13 @@ echo "front-page.php";
 get_header();
 ?>
 
+<?php while ( have_posts() ) : the_post();
+
+    get_template_part( 'template-parts/comp/description', 'detail' );
 
 
-
+endwhile; // End of the loop.
+?>
 
 
 
