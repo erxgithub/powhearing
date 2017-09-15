712f339e6ee7bd23ef2f1798ce9e7db425457000
shovalsolo <shovalso@gmail.com>
2017-09-15 10:21:30 -0400

    removed gitignore from other folders

---
+++ wp-content/themes/powhearing/page-individuals.php
@@ -0,0 +1,25 @@
+<?php
+/* Template Name: individuals */
+echo "page-individuals.php";
+?>
+
+<?php while ( have_posts() ) : the_post();
+
+    get_template_part( 'template-parts/comp/description', 'detail' );
+
+endwhile; // End of the loop.
+?>
+
+<?php
+if(get_field('indiv-title'))
+{ ?>
+
+    <h1 class="organi-title">
+    <?php echo '<p>' . get_field('indiv-title') . '</p>'; ?>
+    </h1>
+
+    <p class="organi-content">
+    <?php echo '<p>' . get_field('indiv-content') . '</p>'; ?>
+    </p>
+
+<?php } ?>
