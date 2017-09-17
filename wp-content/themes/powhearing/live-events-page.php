48875931bb3cab7d3a8b580d5d651d9baf279e55
shovalsolo <shovalso@gmail.com>
2017-09-17 13:32:04 -0400

    description component finished without styling removed comment

--- wp-content/themes/powhearing/live-events-page.php
+++ wp-content/themes/powhearing/live-events-page.php
@@ -3,7 +3,7 @@
 echo "live-events-page.php <br>";
 ?>
 
-// getting template_part that is setting for each page his content
+<!-- getting template_part that is setting for each page his content -->
 
 <?php 
     get_template_part( 'template-parts/comp/description', 'detail' );
--- wp-content/themes/powhearing/page-individuals.php
+++ wp-content/themes/powhearing/page-individuals.php
@@ -3,7 +3,7 @@
 echo "page-individuals.php <br>";
 ?>
 
-// getting template_part that is setting for each page his content
+<!-- getting template_part that is setting for each page his content -->
 
 <?php 
     get_template_part( 'template-parts/comp/description', 'detail' );
