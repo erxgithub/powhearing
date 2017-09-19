2b50e8ce2225d2ccfb3e30b3db0896205a712734
shovalsolo <shovalso@gmail.com>
2017-09-19 15:58:55 -0400

    adding what we do component

--- wp-content/themes/powhearing/front-page.php
+++ wp-content/themes/powhearing/front-page.php
@@ -10,15 +10,10 @@ get_header();
     <?php get_template_part( 'template-parts/comp/what-we-do', 'detail' );?>
 </div>   
 
-<?php $image = wp_get_attachment_image_src(get_field('thx_footer'), 'full'); ?>
-        
-<div class="quote_logo">
-      <img src="<?php echo $bottom; ?>" alt="<?php echo get_the_title(get_field('image_test')) ?>" />
-</div>
+
 
 <!-- About section-->
-<?php
-get_template_part( 'template-parts/comp/modal-form', 'detail' );
+<?php get_template_part( 'template-parts/comp/modal-form', 'detail' );
 
 get_footer(); 
 ?>
