74b216136a54b0df73b0599b5002a2877a147b48
Eric Gregor <erxlink@gmail.com>
2017-09-21 09:32:35 -0400

    Update scenarios-comp.php

--- wp-content/themes/powhearing/template-parts/comp/scenarios-comp.php
+++ wp-content/themes/powhearing/template-parts/comp/scenarios-comp.php
@@ -8,9 +8,8 @@
 <?php while ( have_posts() ) : the_post(); ?>
         <?php
         $scenarios = get_post_meta(get_the_ID(), "scenarios_repeat_group");
-        var_dump($scenarios);
+        //var_dump($scenarios);
         //echo nl2br("\n");
-        //
 
         if ($scenarios[0] !== null) {
             foreach($scenarios[0] as $key => $scenario)
