46c3c228d559346f9dda21dfa8ef5ff43cf088a0
erxgithub <erxlink@gmail.com>
2017-09-21 23:29:32 -0400

    Added styling for services section of organizations, individuals, and live events pages.

--- wp-content/themes/powhearing/template-parts/comp/individual-services.php
+++ wp-content/themes/powhearing/template-parts/comp/individual-services.php
@@ -5,34 +5,40 @@
         $individual_services = get_post_meta(get_the_ID(), "consultation_services_repeat_group");
 
         if ($individual_services[0] !== null) {
+        ?>
+            <div class="services-section">
+                <div>
+                    <h2><?php echo $individual_services[0][0]["title"]; ?></h2>
+                </div>
+
+                <div class="services-grid">
+        <?php
         foreach($individual_services[0] as $key => $individual_service)
         {
             ?>
+                    <div class="service-box">
+                        <div class="service-image">
+                            <img src="<?php echo $individual_service["image-cs"]; ?>" />
+                        </div>
 
-            <div class="individual-services">
-                <div>
-                    <h2><?php echo $individual_service["title"]; ?></h2>
-                </div>
+                        <div class="service-button">
+                            <button class="button modalInfo"
+                                data-open="modalInfo" data-title="<?php echo $individual_service["title-cs"]?>"
+                                data-img="<?php echo $individual_service["image-cs"]?>"
+                                data-content="<?php echo $individual_service["title-content"]?>">
 
-                    <div>
-                        <img src="<?php echo $individual_service["image-cs"]; ?>" />
+                                <?php echo $individual_service["title-cs"]; ?>
+                            </button>
+                        </div>
                     </div>
+            <?php
+        } ?>
 
-                <div>
-                        <button class="button modalInfo" 
-                        data-open="modalInfo" data-title="<?php echo $individual_service["title-cs"]?>"
-                        data-img="<?php echo $individual_service["image-cs"]?>" data-content="<?php echo $individual_service["title-content"]?>">
-
-                            <?php echo $individual_service["title-cs"]; ?>
-                        </button>
                 </div>
             </div>
-
-            <?php
-        }
+    <?php
     }
 
     ?>
 
 <?php endwhile; // end of the loop. ?>
-
